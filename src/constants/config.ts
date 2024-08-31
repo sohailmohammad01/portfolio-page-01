7a288b8fed49d982fa55029196b032a1c9a2632b
Mohammad Sohail <95677810+sohailmohammad01@users.noreply.github.com>
2024-08-31 23:31:40 +0530

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,12 +40,12 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Sohail — 3D Portfolio",
+    fullName: "Sohail",
+    email: "sohailmohammad815@gmail.com",
   },
   hero: {
-    name: "John Doe",
+    name: "Sohail",
     p: ["I develop 3D visuals, user", "interfaces and web applications"],
   },
   contact: {
